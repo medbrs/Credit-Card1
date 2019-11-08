8d615729fcbbf77633ce5506ccd1ce0ea6c82e60
medbrs <ireliaever@gmail.com>
2019-11-08 12:38:19 +0100

    my-card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className='card'>
+            <h1>CREDIT CARD</h1>
+            <img src='https://cdn.freebiesupply.com/logos/large/2x/chip-1-logo-png-transparent.png' alt='ss' width='60px' />
+            <div className='card-infos'>
+                <div className='infos'>
+                    <p className='num'>7253   5648   5848  7548</p>
+                    <div className='container'>
+                        <p className='pin'>5422</p>
+                        <div>
+                            <p className='v-t'>MONTH/YEAR</p>
+                            <div className='valid-thru'>
+                                <p className='v-t'>VALID<br />THRU</p>
+                                <p>></p>
+                                <p>11/50</p>
+                            </div>
+                        </div>
+                    </div>
 
-export default App;
+                    <p className='holder'>CARDHOLDER</p></div>
+                <img className ='imgg' src='https://krebsonsecurity.com/wp-content/uploads/2012/03/mcvisa.png' alt='sss' width='80px' />
+
+            </div>
+        </div>
+    )
+
+}
+export default App
